63eb104fb84660be0ad09ea9513543706cb741ca
shitalkumar58627 <53770105+shitalkumar58627@users.noreply.github.com>
2021-03-11 17:50:09 +0530

    added some text

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
             if (isset($_POST['login']) && !empty($_POST['username']) 
                && !empty($_POST['password'])) {
 				
-               if ($_POST['username'] == 'Devops' && 
+               if ($_POST['username'] == 'Shitalkumar' && 
                   $_POST['password'] == '1234') {
                   $_SESSION['valid'] = true;
                   $_SESSION['timeout'] = time();
